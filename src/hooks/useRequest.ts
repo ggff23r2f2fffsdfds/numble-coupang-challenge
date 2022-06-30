889698a174736b71be07190b2fdcd91043867c18
SungYeolha <tjddufgk@gmail.com>
2022-06-30 18:22:09 +0900

    refactor useRequest custom hooks

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,21 @@
-export const useRequest = () => {};
+import { useQuery } from 'react-query';
+import { UserService } from '../services';
+
+type Options = {
+  refetchInterval?: number;
+};
+
+const queryUserService = {
+  me: UserService.me,
+};
+
+export const useRequest = (
+  key: keyof typeof queryUserService,
+  options?: Options
+) => {
+  const { data, isLoading, isError } = useQuery(key, queryUserService[key], {
+    ...options,
+  });
+
+  return { data, isLoading, isError };
+};
