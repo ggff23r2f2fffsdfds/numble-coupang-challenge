cb1214dd51a288e131b0f5996ea3579e4437f9ae
SungYeolha <tjddufgk@gmail.com>
2022-07-11 03:11:27 +0900

    add Button component

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,8 @@
-import styled from "@emotion/styled";
-import { useForm } from "react-hook-form";
+import Image from 'next/image';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+import { useForm } from 'react-hook-form';
+import { Button } from '../../src/components/Common';
 
 export default function LoginPage() {
   const { register, handleSubmit } = useForm<{
@@ -10,15 +13,53 @@ export default function LoginPage() {
   const onSubmit = () => {};
 
   return (
-    <Wrapper>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("email")} />
-        <input {...register("password")} />
-        <button type="submit">로그인</button>
-        <a href="/auth/signup">회원가입</a>
-      </form>
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <Header>
+          <Image
+            src={'/images/logo_coupang.png'}
+            alt="Logo"
+            width={200}
+            height={50}
+          />
+        </Header>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('email')} placeholder={'아이디(이메일)'} />
+          <input {...register('password')} placeholder={'비밀번호'} />
+          <Button type="submit" color="blue">
+            로그인
+          </Button>
+          <Link href="/auth/signup">
+            <Button color="white">회원가입</Button>
+          </Link>
+        </Form>
+      </Wrapper>
+    </Container>
   );
 }
 
-const Wrapper = styled.div``;
+const Container = styled.div`
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  margin-bottom: 30px;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  gap: 12px 0;
+  flex-direction: column;
+  a {
+    display: block;
+    width: 100%;
+  }
+`;
---
+++ src/components/Common/Button.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css, SerializedStyles } from '@emotion/react';
+import colors from '../../constants/colors';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  color: 'white' | 'blue';
+  type?: 'button' | 'submit';
+  disabled?: boolean;
+};
+
+const COLORS = {
+  white: css`
+    color: ${colors.BLUE};
+    background: ${colors.WHITE};
+  `,
+  blue: css`
+    color: ${colors.WHITE};
+    background: ${colors.BLUE};
+  `,
+};
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  color,
+  type = 'button',
+  disabled = false,
+}) => {
+  const buttonColor = COLORS[color];
+
+  return (
+    <Container type={type} buttonColor={buttonColor} disabled={disabled}>
+      {children}
+    </Container>
+  );
+};
+export default Button;
+
+const Container = styled.button<{ buttonColor: SerializedStyles }>`
+  display: block;
+  padding: 16px 0;
+  text-align: center;
+  font-size: 17px;
+  line-height: 20px;
+  outline: none;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  box-shadow: inset 0 -2px 0 rgb(0 0 0 / 38%);
+  ${(props) => props.buttonColor};
+`;
