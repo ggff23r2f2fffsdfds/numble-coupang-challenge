133689b148f3b352b5d1a6adb17c499c697ca20a
Yeolha Sung <tjddufgk@gmail.com>
2022-08-02 03:04:56 +0900

    feat: add useRequest

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,16 @@
-export const useRequest = () => {};
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useRequest = <T>(url: string) => {
+  const [data, setData] = useState<T>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res: any = await axios.get(`http://localhost:3000/api/${url}`);
+      setData(res.data);
+    };
+    fetchData();
+  }, [url]);
+
+  return data;
+};
